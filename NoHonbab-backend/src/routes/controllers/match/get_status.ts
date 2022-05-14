260cd6d405229d52e73321e2b3a7755416604dcf
jaeyeol <jaeyeolchoi@gmail.com>
2022-05-14 20:53:06 +0900

    backend GET /status

--- NoHonbab-backend/src/routes/controllers/match/get_status.ts
+++ NoHonbab-backend/src/routes/controllers/match/get_status.ts
@@ -1,5 +1,45 @@
 import { Request, Response, NextFunction } from "express";	
 
+import { User } from "../../../entities";
+
 export default async (req: Request, res: Response, next: NextFunction) => {
-	
+	try {
+		const user = await User.findOne({
+			where: {
+				nickname: req.body.nickname,
+			}
+		});
+		if (!user) {
+			return res.status(470).json({
+				code: 470,
+				message: 'non registered nickname',
+			});
+		}	
+		if (user.is_matched == true) {
+			const partnerUser = await User.findOne({
+				where: {
+					partner: {
+						id: user.id
+					}
+				}
+			});
+			return res.status(202).json({
+				code: 202,
+				partner: partnerUser,
+			});
+		}
+		else {
+			return res.status(201).json({
+				code: 201,
+				partner: null,
+			})
+		}
+	}
+	catch (err) {
+		console.error(err);
+		return res.status(408).json({
+			code: 408,
+			message: 'server error',
+		});
+	}
 };
